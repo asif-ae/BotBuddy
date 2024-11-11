244b476f3aaad2d6ef8fd935bfa65912d845978b
Asif Estiak <me.asif.ae@gmail.com>
2024-11-11 19:06:32 +0600

    feat: initialized project

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,20 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [
+    `${__dirname}/src`, // Specify only the main code folder if necessary
+  ],
+  resolver: {
+    blacklistRE: /node_modules\/.*/, // Exclude node_modules if it's not required
+  },
+  server: {
+    enhanceMiddleware: (middleware) => {
+      middleware.poll = true; // Enables polling instead of direct file watching
+      middleware.interval = 1000; // Set the interval to 1 second for reduced load
+      return middleware;
+    },
+  }
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
